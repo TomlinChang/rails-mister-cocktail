3a329d277699a35b2db5d617421604bff1aee929
tom chang <tomlin83@gmail.com>
2020-11-26 14:49:36 +0800

    completed finishing create in doses

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts "destroying all existing record"
+Cocktail.destroy_all
+
+puts "creating new database"
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_raw = open(url).read
+ingredient_json = JSON.parse(ingredient_raw)
+
+ingredient_json["drinks"].each_with_index do |name, index|
+  ingredients = Ingredient.new(name: name["strIngredient1"])
+  ingredients.save
+  break if index == 10
+end
+
+
+puts "Seeding completed"
