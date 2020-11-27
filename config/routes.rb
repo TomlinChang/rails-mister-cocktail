39a7ae70b5b84b7b104ca16c6464a6e8481fbf27
tom chang <tomlin83@gmail.com>
2020-11-27 14:08:45 +0800

    created cloudinary

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   resources :cocktails, only: [ :index, :new, :create, :show ] do
     resources :doses, only: [ :new, :create]
   end
   resources :doses, only: [ :destroy ]
+  resources :users, only: [ :index, :new, :create]
 end
