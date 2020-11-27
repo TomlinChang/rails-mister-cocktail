17d5d546928b4a0e197ae5d535bacfff756b26e6
tom chang <tomlin83@gmail.com>
2020-11-27 15:48:43 +0800

    added root in routes and edited index html

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'users/index'
   get 'users/show'
+  root to: "cocktails#index"
   resources :cocktails, only: [ :index, :new, :create, :show ] do
     resources :doses, only: [ :new, :create]
   end
